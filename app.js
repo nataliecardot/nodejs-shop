a282f6d11dc9ed13e46a927e7314456f125279a0
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-19 11:29:16 -0800

    Send server response with vanilla Node.js

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const http = require('http');
 
 // Takes req listener, a function that will execute for every incoming request. It receives req and res object. Req for incoming message that allows us to receive data from request and res object which we can use to return a response to whoever sent the request
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  // write method allows us to write data to respone, and it works in chunks/line by line
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  // Tells Node we're done creating the response
+  res.end();
 });
 
 server.listen(3000);
