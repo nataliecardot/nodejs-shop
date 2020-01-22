9af74445a821b6c8240b8259aab0ab723ff9b006
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-21 22:02:09 -0800

    Redirect request

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 // Root file that makes up Node.js application (sometimes also called server.js)
 
 // Takes req listener, a function that will execute for every incoming request. It receives req and res object. Req for incoming message that allows us to receive data from request and res object which we can use to return a response to whoever sent the request
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     // write method allows us to write data to respone, and it works in chunks/line by line
     res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
+    res.write('<head><title>Enter message</title></head>');
     // Buttom with type submit in a form sends new request
     // Form action is URL the request that is generated automatically on button click should be sent to
     res.write(
@@ -17,6 +19,14 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     // Tells Node we're done creating the response
     // return is needed to return from the anonymous function in order to not continue the code below the if statement (not to return the response) (must not write or do anything after res.end())
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    // Creates file with submitted text
+    fs.writeFileSync('message.txt', 'DUMMY');
+    // The HTTP 302 Found redirect status response code indicates that the resource requested has been temporarily moved to the URL given by the Location header
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
