7c20f2c967581e96836a7defd347bdedf6ad14fb
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-30 22:44:24 -0800

    Fix bug

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const routes = require('./routes');
 
 // Root file that makes up Node.js application (sometimes also called server.js)
 
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
