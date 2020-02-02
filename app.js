8a310bc387e899bf760160d80b80686920215746
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-01 23:32:25 -0800

    Add middleware (#60)

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require('express');
 
 const app = express();
 
+// The callback will be executed for every incoming request
+// Next is a function passed as an argument to the use method's callback function hy Express, and it has to be executed to allow the request to travel on to next middleware in line. If we don't call next, should send a response, because otherwise the request can't continue its journey
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+});
+
+// using the express function stored in app constant as request handler
 const server = http.createServer(app);
 
 server.listen(3000);
