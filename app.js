2442f01ce4a67ea951a63bdfdd39be7ebf27c83f
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-01 23:45:19 -0800

    Send response (#61)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // The callback will be executed for every incoming request
-// Next is a function passed as an argument to the use method's callback function hy Express, and it has to be executed to allow the request to travel on to next middleware in line. If we don't call next, should send a response, because otherwise the request can't continue its journey
+// Next is a function passed as an argument to the use method's callback function hy Express, and it has to be executed to allow the request to travel on to next middleware in line. If we don't call next, should send a response, because otherwise the request can't continue its journey (and note you can only send one response per request. If you were to call next() after sending a response, the next middleware would be fired, but the response in that one wouldn't be sent)
 app.use((req, res, next) => {
   console.log('In the middleware!');
   next();
@@ -13,6 +13,13 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware!');
+  // send is Express utility function that allows us to send response, and attach a body of type any; Express sets content type automatically (but can still set one manually with Node.js setHeader method)
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.use((req, res, next) => {
+  console.log('Test');
+  res.send('<h1>Hello from Eess!</h1>');
 });
 
 // using the express function stored in app constant as request handler
