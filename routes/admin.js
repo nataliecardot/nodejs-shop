081f8b73d521cc61cc905acabf07635a6f1a4bd4
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-12 18:27:02 -0800

    Convert add-product HTML file to Pug template

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product => POST
