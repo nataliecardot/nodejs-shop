4fb7b39a9608a634f33984a79755e7b561fb4b93
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-12 17:27:06 -0800

    Output dynamic content with Pug (#81)

--- routes/shop.js
+++ routes/shop.js
@@ -9,10 +9,12 @@ const router = express.Router();
 
 // Using get so that the order doesn't matter in app.js (then it's an exact match, unlike with router.use [same for app.use])
 router.get('/', (req, res) => {
+  const products = adminData.products;
   // This is provided by Express. It will use the default templating engine (which is why it was necessary to define it in app.js) then return that template
   // Since already specified that all views are in the views folder, don't have to construct a path to to that folder
   // Don't need shop.pug (extension) since that engine was defined as the default templating engine; it will look for Pug files
-  res.render('shop');
+  // To inject products into template in order to use it in template file shop.pug, passing second argument to render method. The render method allows to pass in data that should be added into the view
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
