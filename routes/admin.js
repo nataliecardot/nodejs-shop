146ee5d859d25a526250baa72bf9f54c4843ce07
Natalie-Cardot <nataliecardot@gmail.com>
2020-03-15 12:35:54 -0700

    Add edit product POST route (#125)

--- routes/admin.js
+++ routes/admin.js
@@ -19,4 +19,6 @@ router.post('/add-product', adminController.postAddProduct);
 // Using dynamic path segment
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+router.post('/edit-product');
+
 module.exports = router;
