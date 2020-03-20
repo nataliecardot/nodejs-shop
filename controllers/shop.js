3a58da67025c8a767644d6cbfebe15b0e3b5b58a
Natalie-Cardot <nataliecardot@gmail.com>
2020-03-19 20:31:00 -0700

    Display cart items on page (#129)

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,9 +43,23 @@ exports.getIndex = (req, res) => {
 };
 
 exports.getCart = (req, res) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
--- models/cart.js
+++ models/cart.js
@@ -62,14 +62,14 @@ module.exports = class Cart {
     });
   }
 
-  // static getCart(cb) {
-  //   fs.readFile(p, (err, fileContent) => {
-  //     const cart = JSON.parse(fileContent);
-  //     if (err) {
-  //       cb(null);
-  //     } else {
-  //       cb(cart);
-  //     }
-  //   });
-  // }
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };
