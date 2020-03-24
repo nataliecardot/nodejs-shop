0617da5cdd84940342e79cabbe9cf54ca07a4a34
Natalie-Cardot <nataliecardot@gmail.com>
2020-03-23 20:27:04 -0700

    Remove comments

--- app.js
+++ app.js
@@ -24,13 +24,11 @@ const shopRoutes = require('./routes/shop');
 // When extended property is set to true, the URL-encoded data will be parsed with the qs library. qs library allows you to create a nested object from your query string. However, my purpose of using this instead of extended: false to get rid of [Object: null prototype] in console (which appears because with that setting, it's parsed by query-string library. The object returned by the querystring.parse() method does not prototypically inherit from the JavaScript Object. This means that typical Object methods such as obj.toString(), obj.hasOwnProperty(), and others are not defined and will not work. In other words, they have null prototype)
 app.use(bodyParser.urlencoded({ extended: true }));
 // Static method that ships with Express is a built-in method that serves static files. Files served statically: not handled by Express.js router or other middleware, but instead directly forwarded to the file system. Path to folder to be served statically is passed in; a folder to grant read access to. Can do this for CSS, JS, images...
-// Reminder: __dirname, a core Node.js feature, gives the absolute path of the directory containing the currently executing file (root folder)
-// Could register multiple static folders, and it would funnel the request through all of them until it were to have a first hit for the file it were looking for
+// __dirname, a core Node.js feature, gives the absolute path of the directory containing the currently executing file (root folder)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // The order of these doesn't matter since using router.get rather than router.use; with get, post, etc., it's an exact match
-// Addition of '/admin' makes it so only routes starting with /admin will go into the admin routes file, and Express will omit/ignore that segment in the URL when it tries to match the routes in the routes file; it's like stripping it out (so you don't have to keep repeating it in each route)
-// So the filtering mechanism allows us to add a common starting segment for our path, which all routes in a given file use, to outsource that into this file so don't have to repeat it for all the routes in the route file
+// Addition of '/admin' makes it so only routes starting with /admin will go into the admin routes file, and Express will omit/ignore that segment in the URL when it tries to match routes in routes file
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
