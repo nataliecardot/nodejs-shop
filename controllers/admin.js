2ad52124a6fe39dc09d182a5a3ed780a0039d6ad
Natalie-Cardot <nataliecardot@gmail.com>
2020-03-29 13:12:27 -0700

    Fetch admin products with Sequelize (#157)

--- controllers/admin.js
+++ controllers/admin.js
@@ -77,13 +77,15 @@ exports.postEditProduct = (req, res) => {
 };
 
 exports.getProducts = (req, res) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res) => {
