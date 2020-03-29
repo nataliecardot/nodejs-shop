137a02ed4ca7a874cb190d575b4167902ab21ce8
Natalie-Cardot <nataliecardot@gmail.com>
2020-03-28 21:53:22 -0700

    Connect to database (#150)

--- util/database.js
+++ util/database.js
@@ -1,16 +1,13 @@
-const mysql = require('mysql2');
+// Connect Sequelize to database. (It uses MySQL2 behind the scenes. It would create a connection pool, as was previously done manually.)
 
-// There are 2 ways of connecting to a SQL database: 1) Set up one connection [to database server] to use for running queries (closing and repening it over and over). Downside: Must reexecute code to create the connection for every new query, and many queries (fetch data, write data, delete data); creating new connections all the time is inefficient, both in code and with regard to performance cost connecting to database 2) Connection pool: better alternative; you do not open and close a single connection to your database, but instead create a "pool" of open connections to reuse. Once a single query is done executing, it puts the connection back into the pool and it becomes available for use again. The connections in the pool aren't opened and closed, but checked in/out
-const pool = mysql.createPool({
-  // Info about database engine/host connecting to
-  // Host: server IP address/name (localhost since running on own machine)
-  host: 'localhost',
-  user: 'root',
-  // host and user provides access to database server, but that server typically has multiple databases
-  // Schema with custom name created in MySQL
-  database: 'node-complete',
-  password: 'unicorns for life'
+// Naming it with a capital 'S' because importing a constructor function/class
+const Sequelize = require('sequelize');
+
+// Create new Sequelize instance, passing args to constructor function (schema name, root username, password)
+// Options object: dialect - make it clear that connecting to MySQL database, since different SQL engines/databases use slightly different SQL syntax. host - localhost would be used by default, but setting it explicitly
+const sequelize = new Sequelize('node-complete', 'root', 'unicorns for life', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-// The promise part allows using promises when working with these connections; get promises when executing queries
-module.exports = pool.promise();
+module.exports = sequelize;
