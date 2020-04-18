10b293059458d6f43ee4260bbe9a1a04c80bd316
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-18 10:26:09 -0700

    Update user to add product to cart (#195)

--- models/user.js
+++ models/user.js
@@ -4,14 +4,29 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  construction(username, email) {
+  construction(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection('users').updateOne(
+      { _id: new ObjectId(this._id) },
+      // Overwrite old cart
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
