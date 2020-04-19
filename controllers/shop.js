99e9d2d63e004260abc78eb21bc48e6f18eacfc1
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-19 13:52:22 -0700

    Display orders for current user (#203)

--- controllers/shop.js
+++ controllers/shop.js
@@ -103,7 +103,7 @@ exports.postOrder = (req, res) => {
 
 exports.getOrders = (req, res) => {
   req.user
-    .getOrders({ include: ['products'] })
+    .getOrders()
     .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
--- models/user.js
+++ models/user.js
@@ -113,6 +113,14 @@ class User {
       });
   }
 
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return (
