8d950c0efd1f0f8f12b58001640198daca8134e5
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-20 20:37:23 -0700

    Create product schema (#211)

--- models/product.js
+++ models/product.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
