9fccdae3cb1530ed5481797b56e191ae4524a172
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-23 18:21:21 -0700

    Get and display orders (#226)

--- controllers/shop.js
+++ controllers/shop.js
@@ -117,8 +117,7 @@ exports.postOrder = (req, res) => {
 };
 
 exports.getOrders = (req, res) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
--- routes/shop.js
+++ routes/shop.js
@@ -22,6 +22,6 @@ router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.post('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
