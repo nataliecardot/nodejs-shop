f88218e252dfa3084be65fa85ae731ea07fbc601
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-24 17:48:36 -0700

    Set cookie (#234)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req, res) => {
+  const isLoggedIn = req.get('Cookie').split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 };
