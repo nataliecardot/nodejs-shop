cc7385ce949e12a7661f3245dd1affa21539fe35
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-27 22:31:21 -0700

    Add signup page (#252)

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,22 +8,36 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Sign Up',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('5e9ff0fb5485a71f44e73136')
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save((err) => {
-        console.log(err);
+        if (err) {
+          console.log(err);
+        }
         res.redirect('/');
       });
     })
     .catch((err) => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
-    console.log(err);
+    if (err) {
+      console.log(err);
+    }
     res.redirect('/');
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
+router.get('/signup', authController.getSignup);
+
 router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
