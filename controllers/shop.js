e194630f0e58d39b0e1637e4519f41299730f017
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-30 13:35:30 -0700

    Fix order button (#262)

--- controllers/shop.js
+++ controllers/shop.js
@@ -92,7 +92,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user,
         },
         products,
--- models/order.js
+++ models/order.js
@@ -10,7 +10,7 @@ const orderSchema = new Schema({
     },
   ],
   user: {
-    name: {
+    email: {
       type: String,
       required: true,
     },
