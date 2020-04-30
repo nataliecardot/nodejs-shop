49fa632237e3040b1a07c50724222424e724db76
Natalie-Cardot <nataliecardot@gmail.com>
2020-04-29 21:44:09 -0700

    Work on route protection (#257)

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect('/login');
+  }
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
