1fe150a14da24706adc0a489cc7d7f294522b206
Natalie-Cardot <nataliecardot@gmail.com>
2020-05-06 16:58:00 -0700

    Change error message

--- controllers/auth.js
+++ controllers/auth.js
@@ -176,10 +176,11 @@ exports.getNewPassword = (req, res) => {
     resetTokenExpiration: { $gt: Date.now() },
   })
     .then((user) => {
+      // This will be the case if a password reset link is clicked after it was already used, since resetToken and resetTokenExpiration fields are set to undefined after password is reset
       if (!user) {
         req.flash(
           'error',
-          'Oops! That reset password link has already been used. If you still need to reset your password, submit a new request.'
+          'Oops! That reset password link has already been used. If you would still like to reset your password, submit a new request.'
         );
         return res.redirect('/login');
       }
