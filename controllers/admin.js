6d45bd62c912bd5802a961e1781343827665934a
Natalie-Cardot <nataliecardot@gmail.com>
2020-05-12 23:32:31 -0700

    Validation product editing (#301)

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,6 +9,7 @@ exports.getAddProduct = (req, res) => {
     editing: false,
     hasError: false,
     errorMessage: null,
+    validationErrors: [],
   });
 };
 
@@ -20,7 +21,7 @@ exports.postAddProduct = (req, res) => {
     console.log(errors.array());
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/add-product',
+      path: '/admin/edit-product',
       editing: false,
       hasError: true,
       product: {
@@ -30,6 +31,7 @@ exports.postAddProduct = (req, res) => {
         description,
       },
       errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
     });
   }
 
@@ -68,6 +70,7 @@ exports.getEditProduct = (req, res) => {
         product,
         hasError: false,
         errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch((err) => console.log(err));
@@ -79,6 +82,26 @@ exports.postEditProduct = (req, res) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
