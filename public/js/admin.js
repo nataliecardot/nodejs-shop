5331d55aa734bc328a6cb2fce42987aaab9024a0
Natalie-Cardot <nataliecardot@gmail.com>
2020-05-22 15:00:24 -0700

    Manipulate DOM upon product deletion (#350)

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,6 +4,9 @@ const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+  // Pass selector to closest() JS method to get element with closest ancestor of that element type. Want to get article parent of button (btn) element, which was passed as this when deleteProduct called in the view
+  const productElement = btn.closest('article');
+
   // Method supported by browser for sending HTTP requests -- actually for both fetching and sending data. Automatically sent to current host/server unless otherwise specified
   fetch('/admin/product/' + prodId, {
     method: 'DELETE',
@@ -13,7 +16,13 @@ const deleteProduct = (btn) => {
     },
   })
     .then((result) => {
-      console.log(result);
+      return result.json();
+    })
+    // Will have response body originally in the format ReadableStream
+    // Product was deleted on the server, now want to also delete on the DOM so don't have to refresh page to see product gone. With this code, existing page is updated when delete button is clicked
+    .then((data) => {
+      // productElement.parentNode.removeChild(); would be needed if supporting IE, but I only care about modern browser support
+      productElement.remove();
     })
     .catch((err) => console.log(err));
 };
