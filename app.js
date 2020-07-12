a019a1c618cd074801650abe873c7d1f1fa0e9e2
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-11 17:45:26 -0700

    Set secure response headers with Helmet middleware

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const { uuid } = require('uuidv4');
+const helmet = require('helmet');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -50,6 +51,10 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+// Add secure response header(s) with Helmet
+// In my app, in developer tools (in the network tab) I can see it added one additional response header for localhost, Strict-Transport-Security. This HTTP header tells browsers to stick with HTTPS and never visit the insecure HTTP version. Once a browser sees this header, it will only visit the site over HTTPS for the next 60 days
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
