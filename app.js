cb56050b5c6de00c7131ddbfaf1f301b27cbc35a
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-13 13:04:36 -0700

    Comment out unused code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const https = require('https');
+// const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
